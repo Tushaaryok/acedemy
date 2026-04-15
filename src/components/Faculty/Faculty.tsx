06c690ee4fdaa63474f8fe18b6c1f0c425ba6c5e
kothariya Tushar <kothariyatushar24@gmail.com>
2026-04-15 22:42:09 +0530

    Add faculty teacher images and update faculty component

--- src/components/Faculty/Faculty.tsx
+++ src/components/Faculty/Faculty.tsx
@@ -1,9 +1,9 @@
 import './Faculty.css';
 
 const facultyData = [
-  { id: 1, name: 'Ram Sir', subject: 'Maths & Science', exp: '14 Years', initials: 'RS', bio: <>Std 9 & 10: Maths & Science<br/>Std 11 & 12: Account & Stat</>, fb: 'https://www.facebook.com/share/19uF9nXqPV/', ig: 'https://www.instagram.com/ramsinghvihara?igsh=MXJiNTR0cnhpejA1cw==' },
-  { id: 2, name: 'Yashvant Sir', subject: 'English', exp: '14 Years', initials: 'YS', bio: 'Std 9 to 12: English', fb: 'https://www.facebook.com/share/1J2HGLatr6/', ig: 'https://www.instagram.com/yashvant_singh_vihara?igsh=MTJtbG5sem56cDNhdg==' },
-  { id: 3, name: 'Jayesh Sir', subject: 'Social Science', exp: '9 Years', initials: 'JS', bio: <>Std 9 & 10: Social Science<br/>Std 11 & 12: Economics, Sociology, Geography, Psychology</>, fb: 'https://www.facebook.com/share/1BDVtYnq8C/', ig: 'https://www.instagram.com/jb_ahir__8?igsh=MTE2cTl2eTdrenRpaA==' }
+  { id: 1, name: 'Ram Sir', subject: 'Maths & Science', exp: '14 Years', img: '/imgs/ram sir.jpeg', initials: 'RS', bio: <>Std 9 & 10: Maths & Science<br/>Std 11 & 12: Account & Stat</>, fb: 'https://www.facebook.com/share/19uF9nXqPV/', ig: 'https://www.instagram.com/ramsinghvihara?igsh=MXJiNTR0cnhpejA1cw==' },
+  { id: 2, name: 'Yashvant Sir', subject: 'English', exp: '14 Years', img: '/imgs/syam sir.jpeg', initials: 'YS', bio: 'Std 9 to 12: English', fb: 'https://www.facebook.com/share/1J2HGLatr6/', ig: 'https://www.instagram.com/yashvant_singh_vihara?igsh=MTJtbG5sem56cDNhdg==' },
+  { id: 3, name: 'Jayesh Sir', subject: 'Social Science', exp: '9 Years', img: '/imgs/jayesh sir.jpeg', initials: 'JS', bio: <>Std 9 & 10: Social Science<br/>Std 11 & 12: Economics, Sociology, Geography, Psychology</>, fb: 'https://www.facebook.com/share/1BDVtYnq8C/', ig: 'https://www.instagram.com/jb_ahir__8?igsh=MTE2cTl2eTdrenRpaA==' }
 ];
 
 export default function Faculty() {
@@ -17,8 +17,12 @@ export default function Faculty() {
         <div className="faculty-grid">
           {facultyData.map(teacher => (
             <div key={teacher.id} className="faculty-card">
-              <div className="faculty-avatar">
-                {teacher.initials}
+              <div className="faculty-avatar" style={teacher.img ? { padding: 0, overflow: 'hidden' } : {}}>
+                {teacher.img ? (
+                  <img src={teacher.img} alt={teacher.name} style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+                ) : (
+                  teacher.initials
+                )}
               </div>
               <h3 className="faculty-name">{teacher.name}</h3>
               <div className="faculty-badge">
